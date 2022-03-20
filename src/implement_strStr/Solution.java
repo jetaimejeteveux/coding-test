1333d239c1260b0b5257d0f1166bc92b8670f869
jetaimejeteveux <akufirman22@gmail.com>
2022-03-21 00:24:38 +0700

    add implement strStr

---
+++ src/implement_strStr/Solution.java
@@ -0,0 +1,21 @@
+package implement_strStr;
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+      
+         if (haystack == null || needle == null) {
+             return -1;
+         }
+ 
+         if (haystack.equals(needle)) {
+             return 0;
+         }
+         for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+             
+             if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                 return i;
+             }
+         }
+         return -1;
+     }
+ }
